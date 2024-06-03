4657ebe7601e29753f8d651d9a6bec631c6bb61f
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-06-03 19:31:08 -0300

    update métodos

--- promises/promises2.js
+++ promises/promises2.js
@@ -7,13 +7,58 @@ function aleatorio(min, max) {
 
 function esperaAi(msg, time) {
     return new Promise((resolve, reject) => {
-        if (typeof msg !== 'string') reject(false);
+        if (typeof msg !== 'string') {
+            reject(false);
+            return;
+        };
 
         setTimeout(() => {
-            resolve(msg)
+            resolve(msg.toUpperCase() + ' - resolvi a promise');
         }, time)
     });
 
 
 }
 
+
+
+const promises = [
+    esperaAi('Promise 1', aleatorio(1, 5)),
+    esperaAi('Promise 2', aleatorio(1, 5)),
+    esperaAi('Promise 3', aleatorio(1, 5)),
+];
+
+
+const baixaPagina = () => {
+    const emCache = true;
+
+    if (!emCache) {
+        return Promise.reject('Página não foi carregada');
+    } else {
+        return esperaAi('Baixei a página', 3000);
+    }
+};
+
+baixaPagina()
+    .then(resolve => {
+        console.log(resolve);
+    })
+    .catch(e => console.log(e));
+
+// Promise.all(promises)
+//     .then(valor => {
+//         console.log(valor)
+//     })
+//     .catch(error => {
+//         console.log(error)
+//     })
+
+
+// Promise.race(promises)
+// .then(valor => {
+//     console.log(valor)
+// })
+// .catch(error => {
+//     console.log(error)
+// })
+//promise.all promise.race promise.resolve promise.reject
