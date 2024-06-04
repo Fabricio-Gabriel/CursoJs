7d35c4cbff4e3090e1306fbaabc31faeffd809fb
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-06-04 19:06:40 -0300

    refatorando código com fetch

--- ajax/modelo/assets/js/fetch.js
+++ ajax/modelo/assets/js/fetch.js
@@ -10,16 +10,27 @@ document.addEventListener('click', (e) => {
     }
 });
 
-function carregaPag(el) {
+async function carregaPag(el) {
     const href = el.getAttribute('href');
 
-    fetch(href)
-        .then(resolve => {
-            if(resolve.status !== 200) throw new Error('ERRO 400 NOSSO');
-            return resolve.text();
-        })
-        .then(html => carregaResultado(html))
-        .catch(error => console.error(error));
+    try {
+        const resolve = await fetch(href);
+        if(resolve.status !== 200) throw new Error('ERRO 404 NOSSO');
+
+        const html = await resolve.text();
+        carregaResultado(html);
+
+    } catch (error) {
+        console.error(error);
+    }
+
+    // fetch(href)
+    //     .then(resolve => {
+    //         if(resolve.status !== 200) throw new Error('ERRO 400 NOSSO');
+    //         return resolve.text();
+    //     })
+    //     .then(html => carregaResultado(html))
+    //     .catch(error => console.error(error));
 }  
 
 function carregaResultado(response) {
