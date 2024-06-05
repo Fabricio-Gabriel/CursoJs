fce4f563899ec66cbba65c374b880f77875901b2
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-06-05 19:50:44 -0300

    first request JSON

---
+++ ajax/axios/assets/js/axios-fetch.js
@@ -0,0 +1,32 @@
+fetch('pessoas.json')
+    .then(resolve => resolve.json())
+    .then(json => {
+        carregaElementos(json);
+    })
+    .catch(error => console.log(error));
+
+function carregaElementos(json) {
+    const resultado = document.querySelector('.resultado');
+    const table = document.createElement('table');
+    
+
+    for(let pessoa of json) {
+        const tr = document.createElement('tr');
+
+        const tdNome = document.createElement('td');
+        tdNome.innerHTML = pessoa.nome;
+        tr.appendChild(tdNome);
+
+        const tdIdade = document.createElement('td');
+        tdIdade.innerHTML = pessoa.idade;
+        tr.appendChild(tdIdade);
+
+        const tdSal = document.createElement('td');
+        tdSal.innerHTML = pessoa.salario;
+        tr.appendChild(tdSal);
+
+        table.appendChild(tr);
+    }
+
+    resultado.appendChild(table);
+}
