616aac2112ca76ab77cac1b2958e37567b8ff335
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-06-13 11:13:52 -0300

    webpack config

---
+++ webpack/src/index.js
@@ -0,0 +1,9 @@
+class Pessoa {
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
+
+const p1 = new Pessoa('Fabricio', 19);
+alert(p1.nome + p1.idade);
