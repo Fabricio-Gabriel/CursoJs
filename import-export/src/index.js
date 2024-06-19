80967110dfd1316564f3dbd96ac5ba497c0415de
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-06-19 20:06:06 -0300

    update import-export

--- import-export/src/index.js
+++ import-export/src/index.js
@@ -1,5 +1,5 @@
 // import { nome as nome2, sobreNome, idade, soma, Pessoa } from './modulo1.js';
-import Pessoa, { nome, sobreNome, idade, soma} from './modulo1.js';
+import Pessoa, { nome as nome2, sobreNome, idade, soma} from './modulo1.js';
 
 // console.log(nome2, sobreNome, idade);
 
@@ -12,3 +12,5 @@ import Pessoa, { nome, sobreNome, idade, soma} from './modulo1.js';
 const p1 = new Pessoa('Fabricio', 'Gabriel');
 console.log(soma(2, 1));
 alert(p1.nome);
+
+console.log(nome2)
--- import-export/src/modulo1.js
+++ import-export/src/modulo1.js
@@ -12,3 +12,4 @@ export default class Pessoa {
         this.sobreNome = sobreNome;
     }
 }
+
