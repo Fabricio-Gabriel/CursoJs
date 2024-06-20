8dafa57d6a864e6b4200d291d83379c7e1ff52a8
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-06-20 14:14:48 -0300

    update modules js

--- node/modulos/app.js
+++ node/modulos/app.js
@@ -1,17 +1,30 @@
-// const req = require('./mod1');
-// const falaNome = req.falaNome;
+// // const req = require('./mod1');
+// // const falaNome = req.falaNome;
 
-// const { nome, sobreNome, falaNome } = require('./mod1');
+// // const { nome, sobreNome, falaNome } = require('./mod1');
 
-// console.log(nome);
-// console.log(falaNome());
+// // console.log(nome);
+// // console.log(falaNome());
 
+// const path = require('path');
+// const mod1 = require('./mod1');
+// const { Pessoa } = require('./mod1');
+// const { falNome } = require('./mod1');
+
+// const p1 = new Pessoa('Fabricio', 'Branco');
+// p1.alteraCor('pardo');
+// console.log(p1);
+// console.log(mod1.sobreNome);
+// console.log(mod1.nome);
+// falNome();
+
+const Cachorro = require('./mod1');
 const path = require('path');
-const mod1 = require('./mod1');
-const { Pessoa } = require('./mod1');
-const falNome = require('./mod1').falNome;
 
-console.log(mod1.sobreNome);
-console.log(mod1.nome);
-falNome();
+const cachorro = new Cachorro('bruccy');
+console.log(cachorro.latir());
+
+console.log(__filename);
+console.log(__dirname);
+console.log(path.resolve(__dirname, '..', '..', 'axios'));
 
--- node/modulos/mod1.js
+++ node/modulos/mod1.js
@@ -1,18 +1,32 @@
-class Pessoa {
+// class Pessoa {
+//     constructor(nome, cor) {
+//         this.nome = nome;
+//         this.cor = cor;
+//     }
+
+//     alteraCor(color) {
+//         this.cor = color;
+//     }
+    
+// }
+
+// exports.Pessoa = Pessoa;
+
+// let nome = 'Fabrico';
+// let sobreNome = 'Gabriel'
+// function falNome() {
+//     console.log('a');
+// }
+
+// exports.sobreNome = sobreNome;
+// exports.nome = nome;
+// exports.falNome = falNome;
+module.exports = class Cachorro {
     constructor(nome) {
-        this.nome = nome;
+        this.nome = nome;    
+    }
+
+    latir() {
+        console.log(`${this.nome} está latindo`);
     }
 }
-
-exports.Pessoa = Pessoa;
-
-let nome = 'Fabrico';
-let sobreNome = 'Gabriel'
-function falNome() {
-    console.log('a');
-}
-
-exports.sobreNome = sobreNome;
-exports.nome = nome;
-exports.falNome = falNome;
-
