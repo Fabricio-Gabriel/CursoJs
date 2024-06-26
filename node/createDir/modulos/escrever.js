8950b32c8810fa6a868f06a049bdb2581d40e940
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-06-26 13:16:45 -0300

    criando arquivos com fs

---
+++ node/createDir/modulos/escrever.js
@@ -0,0 +1,5 @@
+const fs = require('fs').promises;
+const path = require('path');
+const caminhoArquivo = path.resolve(__dirname, '..', 'teste.txt');
+
+fs.writeFile(caminhoArquivo, 'Frase1\n', { flag: 'a', encoding: 'utf8' } );
